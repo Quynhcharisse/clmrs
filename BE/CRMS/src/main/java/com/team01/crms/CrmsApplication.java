f9361e84ba86c106ffbaa895d3ef330128589e3f
Pham Van Nhu Quynh (K18 HCM) <quynhpvnse182895@fpt.edu.vn>
2025-03-01 22:27:15 +0700

    feat: add new commit

---
+++ BE/CRMS/src/main/java/com/team01/crms/CrmsApplication.java
@@ -0,0 +1,13 @@
+package com.team01.crms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmsApplication.class, args);
+    }
+
+}
